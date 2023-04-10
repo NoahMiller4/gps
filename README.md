45a4a8d7728b0345d53338397bb062a9ced82035
Noah Miller <Noseby@live.ca>
2023-04-10 12:49:25 -0500

    map added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tracking user's location with Geolocation APi and Mapbox.
 
-Click [here]() to view
+Click [here](https://noahmiller4.github.io/gps/) to view
--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,51 @@
 
 'use strict'
 
+const overlay = document.querySelector('.overlay');
+const loading = document.querySelector('.loading');
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm9zZWJ5IiwiYSI6ImNsZzViYjM3bTAxcGEzaHFybDd3MW8wMnYifQ.wk2obNgJ24gbwkd7D-kJtw'
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    pitch: 40,
+    zoom: 16
+});
+
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker({
+    color: '#3898ff'
+});
+
+function getLocation(position) {
+    const { longitude, latitude } = position.coords;
+
+    if (longitude && latitude) {
+        map.setCenter([longitude, latitude]);
+        marker.setLngLat([longitude, latitude]).addTo(map);
+        setTimeout(() => { overlay.style.display = 'none' }, 500)
+    }
+}
+
+function errorHandler(event) {
+    loading.style.animationPlayState = 'paused';
+    console.log(event.message);
+};
+
+const options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+};
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+} else {
+    console.log('Geolocation is not enabled')
+}
